5225285c08472bfee2741ef59decffa934dfb87d
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 17:36:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,10 @@ def long_planeteer_calls(call_array)
   call_array.any? do |call|
     call.length > 4
   end
-
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(strings)
+  strings.find do |item|
+    item == "cheddar" || item == "gouda" || item == "camembert"
+  end
 end
