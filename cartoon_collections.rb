5fa24d5c745fadff7c27b1c2dde09d8229dedc68
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-13 16:18:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_name)
+  dwarf_name.each_with_index do |name|
+    puts dwarf_name.each_with_index
 end
 
 def summon_captain_planet# code an argument here
